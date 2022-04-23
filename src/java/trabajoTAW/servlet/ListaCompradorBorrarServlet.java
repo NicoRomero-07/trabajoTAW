920f52a3d4047957151b4b87993ef8aaa7402abf
nicoZhao1908 <0619971117@uma.es>
2022-04-23 09:54:39 +0200

    Update ListaCompradorBorrarServlet.java

--- src/java/trabajoTAW/servlet/ListaCompradorBorrarServlet.java
+++ src/java/trabajoTAW/servlet/ListaCompradorBorrarServlet.java
@@ -7,11 +7,14 @@ package trabajoTAW.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import trabajoTAW.dao.ListaUsuarioFacade;
+import trabajoTAW.entity.ListaUsuario;
 
 /**
  *
@@ -19,6 +22,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "ListaCompradorBorrarServlet", urlPatterns = {"/ListaCompradorBorrarServlet"})
 public class ListaCompradorBorrarServlet extends HttpServlet {
+    
+    @EJB ListaUsuarioFacade listaUsuarioFacade;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,19 +36,13 @@ public class ListaCompradorBorrarServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ListaCompradorBorrarServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ListaCompradorBorrarServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        String str = request.getParameter("id");
+        
+        ListaUsuario listaComprador = this.listaUsuarioFacade.find(str);
+        this.listaUsuarioFacade.remove(listaComprador);
+        
+        response.sendRedirect(request.getContextPath()+"/ListaCompradorServlet");
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
